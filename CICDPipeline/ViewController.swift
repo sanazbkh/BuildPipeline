daa0a2af2f399666751b26bdae9ce76c1d69f372
sanaz Bahmankhah <Sanaz.bahmankhah@tekever.com>
2022-11-29 12:58:16 +0100

    Added comment.

--- CICDPipeline/ViewController.swift
+++ CICDPipeline/ViewController.swift
@@ -12,6 +12,7 @@ final class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        //
         self.view.backgroundColor = .gray
         configButton()
         setupButton()
